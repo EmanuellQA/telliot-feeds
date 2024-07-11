a26820348eb5a4a2774cbc83027940b693decf71
dsm-ll <diogo@liquidloans.io>
2024-07-11 14:06:41 -0300

    return FETCH_USD_MOCK_PRICE

--- src/telliot_feeds/sources/price/spot/coingecko.py
+++ src/telliot_feeds/sources/price/spot/coingecko.py
@@ -10,6 +10,9 @@ from telliot_feeds.dtypes.datapoint import OptionalDataPoint
 from telliot_feeds.pricing.price_service import WebPriceService
 from telliot_feeds.pricing.price_source import PriceSource
 from telliot_feeds.utils.log import get_logger
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 logger = get_logger(__name__)
@@ -71,6 +74,8 @@ class CoinGeckoSpotPriceService(WebPriceService):
         Note that coingecko does not return a timestamp so one is
         locally generated.
         """
+
+        return float(os.getenv("FETCH_USD_MOCK_PRICE", 1)), datetime_now_utc()
 
         asset = asset.lower()
         currency = currency.lower()
